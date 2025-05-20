38203b7c107fd0e2a7b7b709c220db43d080704c
FMNowacki <nowacki.filip.m@gmail.com>
2025-05-20 16:38:33 +0200

    Update LiveMapVisuallizer.cs

--- C#Scripts/LiveMapVisuallizer.cs
+++ C#Scripts/LiveMapVisuallizer.cs
@@ -6,7 +6,7 @@ using Unity.Robotics.ROSTCPConnector.MessageTypes.Nav;
 using System.Collections.Generic;
 #endif
 
-public class LiveMapVisualizer : MonoBehaviour
+public class IncrementalMapVisualizer : MonoBehaviour
 {
     public GameObject wallPrefab;
     public float wallHeight = 1.5f;
@@ -15,8 +15,10 @@ public class LiveMapVisualizer : MonoBehaviour
     private float resolution;
     private int width, height;
     private Vector3 origin;
-    private List<GameObject> spawnedWalls = new List<GameObject>();
-    private sbyte[] previousData = null;
+    private bool isFirstMessage = true;
+
+    // Track cells already visualized
+    private Dictionary<Vector2Int, GameObject> wallMap = new();
 
     void Start()
     {
@@ -25,61 +27,49 @@ public class LiveMapVisualizer : MonoBehaviour
 
     void OnMapReceived(OccupancyGridMsg msg)
     {
-        // Skip if map hasn't changed
-        if (previousData != null && msg.data.Length == previousData.Length)
-        {
-            bool isSame = true;
-            for (int i = 0; i < msg.data.Length; i++)
-            {
-                if (msg.data[i] != previousData[i])
-                {
-                    isSame = false;
-                    break;
-                }
-            }
-            if (isSame) return;
-        }
-
-        previousData = msg.data;
-
-        // Clear old walls
-        foreach (GameObject wall in spawnedWalls)
-        {
-            Destroy(wall);
-        }
-        spawnedWalls.Clear();
-
         resolution = msg.info.resolution;
         width = (int)msg.info.width;
         height = (int)msg.info.height;
-        origin = new Vector3(
-            (float)msg.info.origin.position.x,
-            0f,
-            (float)msg.info.origin.position.y
-        );
+
+        if (isFirstMessage)
+        {
+            origin = new Vector3(
+                (float)msg.info.origin.position.x,
+                0f,
+                (float)msg.info.origin.position.y
+            );
+            isFirstMessage = false;
+        }
+
+        sbyte[] data = msg.data;
 
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
                 int i = x + y * width;
-                sbyte cell = msg.data[i];
+                sbyte value = data[i];
 
-                if (cell == 100)
-                {
-                    Vector3 pos = new Vector3(
-                        x * resolution,
-                        wallHeight / 2f,
-                        y * resolution
-                    ) + origin;
+                if (value != 100)
+                    continue;
 
-                    GameObject wall = Instantiate(wallPrefab, pos, Quaternion.identity, this.transform);
-                    spawnedWalls.Add(wall);
-                }
+                Vector2Int cellKey = new Vector2Int(x, y);
+                if (wallMap.ContainsKey(cellKey))
+                    continue; // Already visualized
+
+                Vector3 pos = new Vector3(
+                    x * resolution,
+                    wallHeight / 2f,
+                    y * resolution
+                ) + origin;
+
+                if (float.IsNaN(pos.x) || float.IsInfinity(pos.x))
+                    continue;
+
+                GameObject wall = Instantiate(wallPrefab, pos, Quaternion.identity, this.transform);
+                wallMap[cellKey] = wall;
             }
         }
-
-        Debug.Log($"Map updated: {spawnedWalls.Count} walls generated.");
     }
 #endif
 }
